16def66b75067dbbfa16f0f59d981cfde013f76c
Burhandinrather <128671151+Burhandinrather@users.noreply.github.com>
2023-07-07 09:17:18 +0530

    Update Notify_via_email_Cron_Job_Schedule.py

--- Notify_via_email_Cron_Job_Schedule.py
+++ Notify_via_email_Cron_Job_Schedule.py
@@ -7,6 +7,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
 import datetime
+
 # ESTABLISHING CONNECTION WITH THE SNOWFLAKE WAREHOUSE 
 engine = create_engine(URL(
    account='nua76068.us-east-1',
@@ -23,8 +24,8 @@ engine = create_engine(URL(
 def send_mail(df,df1,df2,df3):
     msg = MIMEMultipart()
     msg['Subject'] = 'Audience Bridge QC'
-    msg['From'] = 'ratherburhan101@gmail.com'
-    msg['To'] = ("burhan.din@transmissionagency.com,claire.gardner@transmissionagency.com,aniketk@transmissionagency.com")
+    msg['From'] = 'burhan.din@transmissionagency.com'
+    msg['To'] = ("burhan.din@transmissionagency.com,Neelkamal.sahu@transmissionagency.com,claire.gardner@transmissionagency.com")
     
     df3['updated_date'] = pd.to_datetime(df3['updated_date'])
     df['updated_at'] = pd.to_datetime(df['updated_at'])
@@ -35,18 +36,19 @@ def send_mail(df,df1,df2,df3):
 
     # Check if the date in the DataFrame is equal to last Sunday's date
     updated_date_str = df3['updated_date'].dt.strftime("%Y-%m-%d").iloc[0]
-    updated_at = df['updated_at'].iloc[0]
+    updated_at = df['updated_at'].max() 
     today1 = datetime.datetime.today().date()
+    yesterday = today - datetime.timedelta(days=1)
+    yesterday = yesterday.date()
 
     #if we don't have the latest data present in AB_RAW Table then if block will get executed and will send us a text message
-    if updated_date_str != last_sunday.date().strftime("%Y-%m-%d") and updated_at.date() != today1:
+    if updated_date_str != last_sunday.date().strftime("%Y-%m-%d") and updated_at.date() != today1 and updated_at.date() != yesterday:
         body_text4 = f'We do not have the latest data present till {last_sunday.date().strftime("%Y-%m-%d")}. The data is present till {updated_date_str} as the client has not uploaded the data this week'
-    
-    elif updated_date_str != last_sunday.date().strftime("%Y-%m-%d") and updated_at.date() == today1:
+    elif (updated_date_str != last_sunday.date().strftime("%Y-%m-%d")) and (updated_at.date() == today1 or updated_at.date() == yesterday):
     # This else block will get executed if the client uploaded the data and if we  get the data in the AB_RAW Table
     # Set the email body text to include the dataframe information
         if len(df) > 0:
-            body_text1 = f'The Client has uploaded the data which has been loaded successfully on {df["updated_at"][0]}.\n The total Number of Rows loaded = {df["rows_loaded"][0]}\n Date Range: {df["min_date"][0]} to {df["max_date"][0]}\n The data is not the latest i.e., till {last_sunday.date().strftime("%Y-%m-%d")} \n'
+            body_text1 = f'The Client has uploaded the data which has been loaded successfully on {updated_at}.\n The total Number of Rows loaded = {df["rows_loaded"][0]}\n Date Range: {df["min_date"][0]} to {df["max_date"][0]}\n The data is not the latest i.e., till {last_sunday.date().strftime("%Y-%m-%d")} \n'
         else:
             body_text1 = ''
             
@@ -83,7 +85,7 @@ def send_mail(df,df1,df2,df3):
     # This else block will get executed if we have got the latest data present in the AB_RAW Table
     # Set the email body text to include the dataframe information
         if len(df) > 0:
-            body_text1 = f'The latest Data has been loaded successfully on {df["updated_at"][0]}.\n The total Number of Rows loaded = {df["rows_loaded"][0]}\n Date Range: {df["min_date"][0]} to {df["max_date"][0]}\n and is upto date till {last_sunday.date().strftime("%Y-%m-%d")}\n'
+            body_text1 = f'The latest Data has been loaded successfully on {updated_at}.\n The total Number of Rows loaded = {df["rows_loaded"][0]}\n Date Range: {df["min_date"][0]} to {df["max_date"][0]}\n and is upto date till {last_sunday.date().strftime("%Y-%m-%d")}\n'
         else:
             body_text1 = 'No data found for the first dataframe'
             
@@ -117,21 +119,21 @@ def send_mail(df,df1,df2,df3):
                 file_name = os.path.basename(ext_file_location)
                 msg.attach(MIMEApplication(f.read(), Name=file_name))
 
-    if updated_date_str == last_sunday.date().strftime("%Y-%m-%d") and updated_at.date() == today1:
-        body_text5 = f'{body_text1}\n{body_text2}\n{body_text3}\n\nRegards,\nBurhan ud din'
-    elif updated_date_str != last_sunday.date().strftime("%Y-%m-%d") and updated_at.date() == today1: 
-        body_text5 = f'{body_text1}\n{body_text2}\n{body_text3}\n\nRegards,\nBurhan ud din'
+    if updated_date_str != last_sunday.date().strftime("%Y-%m-%d") and updated_at.date() != today1 and updated_at.date() != yesterday:
+        body_text5 = f'{body_text4}\n'
+    elif (updated_date_str != last_sunday.date().strftime("%Y-%m-%d")) and (updated_at.date() == today1 or updated_at.date() == yesterday):
+        body_text5 = f'{body_text1}\n{body_text2}\n{body_text3}\n'
     else: 
-        body_text5 = f'{body_text4}\n\nRegards,\nBurhan ud din'
+        body_text5 = f'{body_text1}\n{body_text2}\n{body_text3}\n' 
 
     body_text = f'Hi Team, \n\n  {body_text5}\n\nRegards,\nBurhan ud din'    
 
     msg.attach(MIMEText(body_text))
             
 
-    with smtplib.SMTP('smtp.gmail.com',587) as smtp:
+    with smtplib.SMTP('smtp.office365.com',587) as smtp:
         smtp.starttls()
-        smtp.login('ratherburhan101@gmail.com', 'xxxxxxxxxxxxxxxxxxxxxxx')
+        smtp.login('burhan.din@transmissionagency.com', 'xxxxxxxxxxxxxxxxxxxxxx')
         smtp.send_message(msg)
         print('Mail sent')
 
@@ -139,7 +141,7 @@ def send_mail(df,df1,df2,df3):
 # FETCHING THE DATA FROM SNOWFLAKE USING SQL AND STORING IT IN A VARIABLE
 
 sql1='''
-select * from AB_INTENT_KW.INBOUND.AB_RAW_INFO;
+select * from AB_INTENT_KW.INBOUND.AB_RAW_INFO ORDER BY ID DESC LIMIT 1;
 '''
 
 sql2='''
@@ -148,9 +150,8 @@ select COUNTRYCODE From AB_INTENT_KW.INBOUND.AB_RAW where COUNTRYCODE IS NOT NUL
 AND date(timestamp) BETWEEN (select MIN_DATE from AB_INTENT_KW.INBOUND.AB_RAW_INFO ORDER BY ID DESC LIMIT 1)
 AND  (select MAX_DATE from AB_INTENT_KW.INBOUND.AB_RAW_INFO ORDER BY ID DESC LIMIT 1));
 ''' 
-
 sql3 = '''
-select KEYWORD from "AB_INTENT_KW"."OUTBOUND"."TOPICS_MAPPING" where CLIENT = 'Maersk' AND KEYWORD NOT IN 
+select KEYWORD from "AB_INTENT_KW"."INBOUND"."TOPICS_MAPPING" where CLIENT = 'Maersk' AND KEYWORD NOT IN 
 (select distinct replace(KWTEXT,'_',' ') AS KEYWORDTEXT from AB_INTENT_KW.INBOUND.AB_RAW
 where date(timestamp) BETWEEN (select MIN_DATE from AB_INTENT_KW.INBOUND.AB_RAW_INFO ORDER BY ID DESC LIMIT 1)
 AND  (select MAX_DATE from AB_INTENT_KW.INBOUND.AB_RAW_INFO ORDER BY ID DESC LIMIT 1))
@@ -170,7 +171,7 @@ df2 = pd.read_sql(sql3, engine)
 df3 = pd.read_sql(sql4, engine)
 #print (df3)
 
-ext_file_location = (r"C:\Work\Audience Bridge\AB_INTENT_KW.AB_RAW_QC\missing_keywords.csv")
+ext_file_location = ("/home/ec2-user/Project/Audience_Bridge/weekly_data_info.csv")
 df2.to_csv(ext_file_location, encoding='utf-8',  index = False, sep=',')    
 
 send_mail(df,df1,df2,df3)
